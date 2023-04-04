25b17449636e6f1f38a14942d1c4b078df22362c
michaelinsana <122934621+michaelinsana@users.noreply.github.com>
2023-04-04 17:14:59 -0400

    Update main.c

--- main.c
+++ main.c
@@ -1,5 +1,5 @@
 #include <msp430.h> 
-#include <Stepper.h>
+#include "Stepper.h"
 
 
 /**
@@ -12,20 +12,81 @@ void initTimer();       //initialize timer
 
 void calibrate();       //circle the motor and get initial values
 
-void search();          //search for the
+int pulse();
 
+int mode = 0;   /*    0 = search
+                      1 = focus   */
 
-
-
-
-
-
+const int MAX_DIST = 300;
 
 
 
 int main(void)
 {
-	WDTCTL = WDTPW | WDTHOLD;	// stop watchdog timer
+	WDTCTL = WDTPW | WDTHOLD;	             // stop watchdog timer
+    PM5CTL0 &= ~LOCKLPM5;                    // Disable the GPIO power-on default high-impedance mode
+
+    //calibrate();              Use after Proof of Concept
+
+
+    while (1) {
+
+        switch (mode) {
+        case 0:                                     //search
+
+            StepperMotor.step(1);
+            int dist = pulse();
+
+            if (dist < MAX_DIST)
+                mode = 1;
+
+            else
+                mode = 0;
+
+            break;
+
+        case 1:                                     //focus
+
+
+
+            if (dist < MAX_DIST)
+
+
+            break;
+
+        }
+
+   }
 	
 	return 0;
+
+}
+
+
+
+void initGPIO() {        //initialize GPIO pin for the motor and ultrasonic
+
+
+
+
+}
+
+
+
+
+void initTimer() {
+
+
+}
+
+
+
+
+int pulse() {
+
+
+
+
+
+}
 }
