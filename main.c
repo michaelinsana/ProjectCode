74874727fee561cf0563344143c8ea7b35c23273
michaelinsana <122934621+michaelinsana@users.noreply.github.com>
2023-05-01 08:31:54 -0400

    Update main.c

--- main.c
+++ main.c
@@ -1,5 +1,10 @@
 #include <msp430.h> 
-#include "Stepper.h"
+
+#define HBRIDGE_PIN1 BIT4 // GPIO pin connected to H-bridge input 1
+#define HBRIDGE_PIN2 BIT5 // GPIO pin connected to H-bridge input 2
+#define HBRIDGE_PIN3 BIT6 // GPIO pin connected to H-bridge input 3
+#define HBRIDGE_PIN4 BIT7 // GPIO pin connected to H-bridge input 4
+
 
 
 /**
@@ -34,7 +39,7 @@ int main(void)
         switch (mode) {
         case 0:                                     //search
 
-            StepperMotor.step(1);
+            step_forward();
             int dist = pulse();
 
             if (dist < MAX_DIST)
@@ -66,9 +71,15 @@ int main(void)
 
 void initGPIO() {        //initialize GPIO pin for the motor and ultrasonic
 
+	P1DIR |= HBRIDGE_PIN1 | HBRIDGE_PIN2 | HBRIDGE_PIN3 | HBRIDGE_PIN4; // Set GPIO pins as outputs
+	
 
+	// Set TRIG_PIN as an output and ECHO_PIN as an input
+   	 P1DIR |= TRIG_PIN;
+   	 P1DIR &= ~ECHO_PIN;
 
-
+    	// Configure the Timer_B0 to count up to 65535 with a clock source of SMCLK
+   	 TB0CTL |= TBSSEL_2 ;
 }
 
 
@@ -79,14 +90,44 @@ void initTimer() {
 
 }
 
+void step_forward() {
+    P1OUT |= HBRIDGE_PIN1; // Set input 1
+    P1OUT &= ~HBRIDGE_PIN2; // Clear input 2
+    P1OUT |= HBRIDGE_PIN3; // Set input 3
+    P1OUT &= ~HBRIDGE_PIN4; // Clear input 4
+}
+
+void step_backward() {
+    P1OUT &= ~HBRIDGE_PIN1; // Clear input 1
+    P1OUT |= HBRIDGE_PIN2; // Set input 2
+    P1OUT &= ~HBRIDGE_PIN3; // Clear input 3
+    P1OUT |= HBRIDGE_PIN4; // Set input 4
+}
+
 
 
 
 int pulse() {
 
+    P1OUT |= TRIG_PIN;
+    __delay_cycles(10);
+    P1OUT &= ~TRIG_PIN;
 
+    // Reset the timer and start counting
+    TB0CTL |= TBCLR | MC_2;
 
+    // Wait for the echo pin to go high
+    while ((P1IN & ECHO_PIN) == 0);
+
+    // Wait for the echo pin to go low
+    while (P1IN & ECHO_PIN);
+
+    // Stop the timer and calculate the time
+    TB0CTL &= ~MC_2;
+    int time_us = TB0R;
+
+    return time_us/58;
 
 
 }
-}
+
